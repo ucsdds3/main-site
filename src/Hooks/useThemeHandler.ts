c64cf64c334711a31561d8c661449e33c29be2d7
Issac Roy <issacroy05@gmail.com>
2025-04-19 15:12:05 -0700

    Theme now saves on reload

--- src/Hooks/useThemeHandler.ts
+++ src/Hooks/useThemeHandler.ts
@@ -5,22 +5,20 @@ export const useThemeHandler = () => {
   const { isDark, setIsDark } = useTheme();
 
   useEffect(() => {
-    const storedTheme = localStorage.getItem("theme");
     const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    setIsDark(storedTheme ? storedTheme == "dark" : mediaQuery.matches);
 
-    // Update on system theme change
     const handleChange = (e: MediaQueryListEvent) => {
       setIsDark(e.matches);
       localStorage.setItem("theme", e.matches ? "dark" : "light");
     };
-    
+
     mediaQuery.addEventListener("change", handleChange);
     return () => mediaQuery.removeEventListener("change", handleChange);
   }, [setIsDark]);
 
   useEffect(() => {
-    document.documentElement.setAttribute("data-theme", isDark ? "dark" : "light");
-    localStorage.setItem("theme", isDark ? "dark" : "light");
+    const theme = isDark ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
   }, [isDark]);
 };
