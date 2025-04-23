9a40f93df362c3e5fa3e6c25928f8f456df6c9ad
Issac Roy <issacroy05@gmail.com>
2025-04-23 13:30:20 -0700

    Feat: Projects

--- src/Pages/Projects/Projects.tsx
+++ src/Pages/Projects/Projects.tsx
@@ -3,6 +3,8 @@ import Page from "../Page/Page";
 import Landing from "./Landing";
 import AboutTeam from "../../Components/AboutTeam";
 import projects from "../../Assets/Data/projects.json";
+import ShowCase from "./Showcase";
+import Gallery from "../../Components/Gallery";
 
 const Projects = () => {
   const scrollRef = useRef<HTMLDivElement>(null!);
@@ -12,6 +14,8 @@ const Projects = () => {
       <Landing />
       <div ref={scrollRef}>
         <AboutTeam {...projects.about} />
+        <ShowCase />
+        <Gallery images={projects.images} />
       </div>
     </Page>
   );
