c316876b6105e19c6d01db3adf0d52929fdfe66e
Issac Roy <issacroy05@gmail.com>
2025-04-23 13:30:20 -0700

    Feat: Projects

--- src/Pages/Board/Board.tsx
+++ src/Pages/Board/Board.tsx
@@ -1,49 +1,13 @@
 import { useState } from "react";
-import { Committee, LinkType, MemberType } from "../../Utils/types";
-import members from "../../Assets/Data/members.json";
+import { CommitteeType } from "../../Utils/types";
 import committees from "../../Assets/Data/committees.json";
 import Page from "../Page/Page";
 import Star from "../../Components/Star";
 import SelectCommittee from "./SelectCommittee";
-// import MemberCard from "./MemberCard";
-import { unbreakable } from "../../Utils/functions";
-import HoverCard from "../../Components/HoverCard";
-import { IoMail } from "react-icons/io5";
-import { FaGlobe, FaLinkedin } from "react-icons/fa6";
-import { IoIosDocument } from "react-icons/io";
-
-const formatMemberLinks = ({ email, website, linkedIn, resume }: MemberType) =>
-  [
-    email && {
-      title: "Email",
-      icon: <IoMail />,
-      href: `mailto:${email}`,
-      color: "#F58134",
-    },
-    linkedIn && {
-      title: "LinkedIn",
-      icon: <FaLinkedin />,
-      href: linkedIn,
-      color: "#11B3C9",
-    },
-    resume && {
-      title: "Resume",
-      icon: <IoIosDocument />,
-      href: resume,
-      color: "#434343",
-    },
-    website && {
-      title: "Website",
-      icon: <FaGlobe />,
-      href: website,
-      color: "#222222",
-    },
-  ].filter(Boolean) as LinkType[];
+import MemberCards from "./MemberCards";
 
 const Board = () => {
-  const typedMembers = members as MemberType[];
-  const [committee, setCommittee] = useState<Committee>(Object.keys(committees)[0] as Committee);
-  const filteredMembers = typedMembers.filter((member) => member.committees?.includes(committee));
+  const [committee, setCommittee] = useState<CommitteeType>(Object.keys(committees)[0] as CommitteeType);
 
   return (
     <Page>
@@ -52,29 +16,7 @@ const Board = () => {
         <Star size={1.1} className="absolute top-10 right-14" />
 
         <SelectCommittee committee={committee} setCommittee={setCommittee} />
-
-        <div className="w-full flex flex-col gap-4">
-          <h1 className="text-[clamp(1.8rem,5vw,2.5rem)] font-medium text-center lg:text-left">
-            {"Meet our "}
-            <span className="text-(--color-primary) text-glow">{committee}</span>
-            {committee !== "Alumni" && unbreakable(" Team")}!
-          </h1>
-          <p className="text-[clamp(1.1rem,1.5vw,1.4rem)] font-light px-[clamp(1rem,3vw,5rem)] lg:px-0 indent-8 lg:indent-0">
-            {committees[committee]}
-          </p>
-
-          <div className="grid grid-cols-[repeat(auto-fit,minmax(260px,1fr))] gap-y-8 py-12">
-            {filteredMembers.map((member, index) => (
-              <HoverCard
-                key={index}
-                title={member.name}
-                subtitle={member.role}
-                image={member.image}
-                links={formatMemberLinks(member)}
-              />
-            ))}
-          </div>
-        </div>
+        <MemberCards committee={committee} />
       </div>
     </Page>
   );
---
+++ src/Pages/Projects/ShowCase.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import HoverCard from "../../Components/HoverCard";
+import Section from "../../Components/Section";
+import projectsData from "../../Assets/Data/projects.json";
+
+const ShowCase = () => {
+  const [year, setYear] = useState(2023);
+  const [category, setCategory] = useState("All");
+  const projects = projectsData.projects;
+  const filteredProjects = projects.filter(
+    (project) => project.year === year && (category === "All" || project.category === category)
+  );
+
+  const years = [
+    ...new Set(
+      projects
+        .map((project) => project.year)
+        .sort()
+        .reverse()
+    ),
+  ];
+
+  const categories = [
+    ...new Set(
+      projects
+        .filter((project) => project.year === year)
+        .map((project) => project.category)
+        .sort()
+    ),
+  ];
+
+  return (
+    <Section>
+      <div className="flex justify-between items-center w-full">
+        <div className="flex flex-col">
+          <h2 className="text-2xl font-semibold">PROJECT SHOWCASE</h2>
+          <p>Take a look at some projects from previous years.</p>
+        </div>
+
+        <div className="flex gap-4">
+          <fieldset className="fieldset w-[clamp(10rem,15vw,15rem)] flex flex-col items-center">
+            <span className="text-base font-semibold">Year</span>
+            <select
+              value={year}
+              className="select select-primary"
+              onChange={(e) => setYear(parseInt(e.target.value))}
+            >
+              {years.map((year, index) => (
+                <option key={index}>{year}</option>
+              ))}
+            </select>
+          </fieldset>
+
+          <fieldset className="fieldset w-[clamp(10rem,15vw,15rem)] flex flex-col items-center">
+            <span className="text-base font-semibold">Category</span>
+            <select
+              value={category}
+              className="select select-primary"
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="All">All</option>
+              {categories.map((category, index) => (
+                <option key={index}>{category}</option>
+              ))}
+            </select>
+          </fieldset>
+        </div>
+      </div>
+
+      <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(360px,1fr))] gap-y-8">
+        {filteredProjects.slice(0, 6).map(({ name, image }, index) => (
+          <HoverCard key={index} title={name} size="350px" image={image} />
+        ))}
+      </div>
+    </Section>
+  );
+};
+
+export default ShowCase;
