06951fb03b66c307071e17be1d25083794f06568
Hieu Nguyen <hieun201104@gmail.com>
2025-05-02 18:03:54 -0700

    Feat: aggresive lazy loading of pages

--- src/Pages/Partners/Partners.tsx
+++ src/Pages/Partners/Partners.tsx
@@ -1,17 +1,27 @@
+import { lazy, Suspense } from "react";
 import faq from "../../Assets/Data/FAQ.json";
 import OurPartners from "../../Components/OurPartners";
-import WorkWithUs from "./WorkWithUs";
-import ContactUs from "../../Components/ContactUs";
-import FAQ from "../../Components/FAQ";
+const WorkWithUs = lazy(() => {
+  return import("./WorkWithUs");
+});
+const ContactUs = lazy(() => {
+  return import("../../Components/ContactUs");
+});
+
+const FAQ = lazy(() => {
+  return import("../../Components/FAQ");
+});
 import Page from "../Page/Page";
 
 const Partners = () => {
   return (
     <Page>
       <OurPartners />
-      <WorkWithUs />
-      <ContactUs />
-      <FAQ faq={faq.partners} />
+      <Suspense>
+        <WorkWithUs />
+        <ContactUs />
+        <FAQ faq={faq.partners} />
+      </Suspense>
     </Page>
   );
 };
