7f3e8931a20589beefd088f49406360eb2ba934b
Hieu Nguyen <hieun201104@gmail.com>
2025-05-02 18:03:54 -0700

    Feat: aggresive lazy loading of pages

--- src/Pages/DataHacks/DataHacks.tsx
+++ src/Pages/DataHacks/DataHacks.tsx
@@ -1,12 +1,18 @@
-import { useRef } from "react";
+import { lazy, Suspense, useRef } from "react";
 import { newArray } from "../../Utils/functions.tsx";
 import Page from "../Page/Page";
-import Gallery from "../../Components/Gallery";
 import datahacks from "../../Assets/Data/datahacks.json";
-import About from "../../Components/About";
-import ShowCase from "./ShowCase";
-import Landing from "./Landing";
 
+import Landing from "./Landing";
+const About = lazy(() => {
+  return import("../../Components/About");
+});
+const ShowCase = lazy(() => {
+  return import("./ShowCase.tsx");
+});
+const Gallery = lazy(() => {
+  return import("../../Components/Gallery");
+});
 const DataHacks = () => {
   const scrollRef = useRef<HTMLDivElement>(null!);
   const fakeGallery = newArray(7, "").map(() =>
@@ -17,12 +23,14 @@ const DataHacks = () => {
     <Page scrollRef={scrollRef}>
       <Landing />
       <div ref={scrollRef}>
-        <About {...datahacks.about} />
-        <ShowCase />
-        <Gallery
-          images={datahacks.images || fakeGallery}
-          link="https://drive.google.com/drive/folders/1uHYeanJW0hPyiCUOcI7tPYQRlkWrJnsU"
-        />
+        <Suspense>
+          <About {...datahacks.about} />
+          <ShowCase />
+          <Gallery
+            images={datahacks.images || fakeGallery}
+            link="https://drive.google.com/drive/folders/1uHYeanJW0hPyiCUOcI7tPYQRlkWrJnsU"
+          />
+        </Suspense>
       </div>
     </Page>
   );
