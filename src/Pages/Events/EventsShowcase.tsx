35d47f65cbf94838a4cf0c8bbcc5652d12c7e780
Hieu <vin028@ucsd.edu>
2025-05-12 20:28:19 -0700

    Feat: Resize EventsShowcase

--- src/Pages/Events/EventsShowcase.tsx
+++ src/Pages/Events/EventsShowcase.tsx
@@ -1,13 +1,9 @@
 import Section from "../../Components/Section";
-import datahacks from "../../Assets/Data/datahacks.json";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
-import About from "../../Components/About";
 import useEmblaCarousel from "embla-carousel-react";
 
-const EventsShowCase = () => {
-  const winners = datahacks.winners;
-
+const EventsShowCase = ({ images }: { images: { title: string; image: string }[] }) => {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
   const btnClass =
     "rounded-full w-fit h-fit hover:text-(--color-primary) p-3 hover:bg-base-300 transition-colors duration-300 cursor-pointer text-2xl lg:mx-8";
@@ -21,23 +17,23 @@ const EventsShowCase = () => {
   };
 
   return (
-    <Section className="gap-0 w-full">
-      <div className="w-full  flex items-center">
+    <Section className="gap-0 ">
+      <div className="w-[80vw] h-[30vw] flex items-center">
         <button onClick={handlePrev} className={btnClass} aria-label="Previous">
           <IoIosArrowBack />
         </button>
-        <section className="w-full flex h-fit p-0 overflow-x-hidden" ref={emblaRef}>
-          <div className="w-full h-fit flex flex-row gap-2 md:px-4 ">
-            {winners.map((winner, i) => {
+        <section className="w-full flex h-full p-0 overflow-x-hidden" ref={emblaRef}>
+          <div className="w-full h-full flex flex-row gap-2 md:px-4 ">
+            {images.map((image, i) => {
               return (
-                <div className="flex-shrink-0 flex w-full h-fit p-0">
-                  <About
+                <div className="w-full h-full flex-shrink-0">
+                  <div
                     key={i}
-                    noAbout
-                    name={`${winner.category}: ${winner.title}`}
-                    image={winner.image}
-                    className="w-full h-fit"
-                  />
+                    className="flex-shrink-0 border-2 hover:border-(--color-primary) duration-300 rounded-xl flex flex-col w-full h-full items-center justify-center  p-[clamp(1.5rem,2vw,2.5rem)]"
+                  >
+                    <h1>{image.title}</h1>
+                    <img src={image.image} alt="" className="h-full object-cover" />
+                  </div>
                 </div>
               );
             })}
