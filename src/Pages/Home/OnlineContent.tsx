ed1b4c5c0540524463b235d3f241f8934f3dd2fd
Ansh Bhatnagar <anshbhatnagar1612@gmail.com>
2025-05-13 00:35:05 -0700

    reordering partners

--- src/Pages/Home/OnlineContent.tsx
+++ src/Pages/Home/OnlineContent.tsx
@@ -6,8 +6,8 @@ import onlineContent from "../../Assets/Data/onlineContent.json"
 const OnlineContent = () => {
   return (
     <Section title="Online Content" className="gap-0">
-      <p className="text-lg font-light max-w-xl text-center px-10">
-        Have something to share or want to explore more? Read our latest data science articles or submit your own for publication.
+      <p className="text-2xl font-light max-w-xl text-center px-10">
+        Have something to share or want to explore more? Read our latest articles or submit your own for publication!
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-10">
         {onlineContent.map((content, index) => (
