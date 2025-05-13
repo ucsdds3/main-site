ed95eca43c7c6e5f8cc3e593e256927bd313be7b
Issac Roy <issacroy05@gmail.com>
2025-05-12 21:29:16 -0700

    Too tired, can't think of a commit message

--- src/Pages/Home/OnlineContent.tsx
+++ src/Pages/Home/OnlineContent.tsx
@@ -5,8 +5,11 @@ import onlineContent from "../../Assets/Data/onlineContent.json"
 
 const OnlineContent = () => {
   return (
-    <Section title="Online Content">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <Section title="Online Content" className="gap-0">
+      <p className="text-lg font-light max-w-xl text-center px-10">
+        Have something to share or want to explore more? Read our latest data science articles or submit your own for publication.
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-10">
         {onlineContent.map((content, index) => (
           <BrowserCard
             key={content.title}
