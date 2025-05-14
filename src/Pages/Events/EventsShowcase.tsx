9aa13d74537a4d6b3a281a7dff1fac80b3e078e8
Hieu <vin028@ucsd.edu>
2025-05-13 19:27:06 -0700

    Feat: Made workshops slideshow responsive

--- src/Pages/Events/EventsShowcase.tsx
+++ src/Pages/Events/EventsShowcase.tsx
@@ -2,11 +2,27 @@ import Section from "../../Components/Section";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 import useEmblaCarousel from "embla-carousel-react";
+import AutoScroll from "embla-carousel-auto-scroll";
+import { useEffect } from "react";
 
 const EventsShowCase = ({ images }: { images: { title: string; image: string }[] }) => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
+    AutoScroll({ playOnInit: false, speed: 0.5 }),
+  ]);
+  useEffect(() => {
+    const autoScroll = emblaApi?.plugins()?.autoScroll;
+    const isPhone = window.matchMedia("(max-width: 768px)");
+    if (isPhone.matches) {
+      console.log("play");
+
+      autoScroll?.play();
+    } else {
+      console.log("stop");
+      autoScroll?.stop();
+    }
+  }, [emblaApi]);
   const btnClass =
-    "rounded-full w-fit h-fit hover:text-(--color-primary) p-3 hover:bg-base-300 transition-colors duration-300 cursor-pointer text-2xl lg:mx-8";
+    "rounded-full hidden md:block w-fit h-fit hover:text-(--color-primary) p-3 hover:bg-base-300 transition-colors duration-300 cursor-pointer text-2xl lg:mx-8";
 
   const handlePrev = () => {
     emblaApi?.scrollPrev();
@@ -22,16 +38,16 @@ const EventsShowCase = ({ images }: { images: { title: string; image: string }[]
         <button onClick={handlePrev} className={btnClass} aria-label="Previous">
           <IoIosArrowBack />
         </button>
-        <section className="w-full flex h-full p-0 overflow-x-hidden" ref={emblaRef}>
+        <section className="w-full flex md:h-full h-fit p-0 overflow-x-hidden" ref={emblaRef}>
           <div className="w-full h-full flex flex-row gap-2 md:px-4 ">
             {images.map((image, i) => {
               return (
-                <div className="w-full h-full flex-shrink-0">
+                <div className="w-2/3 md:w-full h-full flex-shrink-0">
                   <div
                     key={i}
-                    className="flex-shrink-0 border-2 hover:border-(--color-primary) duration-300 rounded-xl flex flex-col w-full h-full items-center justify-center  p-[clamp(1.5rem,2vw,2.5rem)] gap-2"
+                    className="flex-shrink-0 border-2 hover:border-(--color-primary) duration-300 rounded-xl flex flex-col w-full h-full items-center justify-center   p-[clamp(0.5rem,2vw,2.5rem)] gap-2"
                   >
-                    <h2 className="text-[clamp(1.5rem,2vw,2.5rem)] font-bold uppercase w-fit">
+                    <h2 className="text-[clamp(1rem,2vw,2.5rem)] min-h-[3rem] font-bold uppercase w-fit text-center">
                       {image.title}
                     </h2>
 
