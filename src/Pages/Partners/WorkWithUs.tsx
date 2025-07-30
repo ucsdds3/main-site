326818aff773f11fa8a66b06f3e22ef1b1fb4113
Issac Roy <issacroy05@gmail.com>
2025-07-29 23:20:45 -0700

    Fix: added consulting link to partners page, closes #83

--- src/Pages/Partners/WorkWithUs.tsx
+++ src/Pages/Partners/WorkWithUs.tsx
@@ -5,10 +5,19 @@ const WorkWithUs = () => {
   return (
     <Section>
       <div className="flex flex-col items-center text-center">
-        <h3 className="text-[clamp(1rem,2.5vw,1.5rem)] font-semibold uppercase">Want to Work With Us?</h3>
+        <h3 className="text-[clamp(1rem,2.5vw,1.5rem)] font-semibold uppercase">
+          Want to Work With Us?
+        </h3>
         <h2 className="text-[clamp(2.2rem,6vw,4rem)] font-semibold uppercase">DS3 Consulting</h2>
       </div>
-      <Button onClick={() => {}}>LEARN MORE</Button>
+      <Button
+        onClick={() => {
+          console.log("clicked");
+          window.open("https://ds3ucsd.com/?site=consulting", "_blank");
+        }}
+      >
+        LEARN MORE
+      </Button>
     </Section>
   );
 };
