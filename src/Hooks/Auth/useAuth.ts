d7d644b530b34c91f8738c86e7d937696e098a13
Issac Roy <issacroy05@gmail.com>
2025-09-09 19:19:20 -0700

    Feat: member home page

--- src/Hooks/Auth/useAuth.ts
+++ src/Hooks/Auth/useAuth.ts
@@ -2,13 +2,22 @@ import { useEffect } from 'react'
 import { AuthState } from '../../Utils/types';
 import { useAuthStore } from './useAuthStore';
 import { supabase } from '../../Utils/supabase';
+import { useSiteHandler } from '../useSiteHandler';
+import { User } from '@supabase/supabase-js';
 
 
 export function useAuth() {
+  const {navigate} = useSiteHandler();
+
   useEffect(() => {
     const getUser = async () => {
       const authState = new URLSearchParams(window.location.search).get("authState") as AuthState;
       if (authState && authState != "authenticated") useAuthStore.setState({ authState });
+
+      const foundUser = (user: User) => {
+        useAuthStore.setState({ user, authState: "authenticated" });
+        navigate({ pathname: "/", subdomain: "members" });
+      }
       
       const tokenHash = new URLSearchParams(window.location.search).get("tokenHash");
       if (tokenHash && tokenHash != "authenticated") {
@@ -16,11 +25,14 @@ export function useAuth() {
           token_hash: tokenHash,
           type: "recovery",
         });
-        if (data?.user) useAuthStore.setState({ user: data.user });
+        if (data?.user) return foundUser(data.user);
       }
 
       const { data } = await supabase.auth.getUser();
-      if (data?.user) useAuthStore.setState({ user: data.user });
+      if (data?.user) return foundUser(data.user);
+      
+      const user = localStorage.getItem("user");
+      if (user) return foundUser(JSON.parse(user));
     };
 
     getUser();
