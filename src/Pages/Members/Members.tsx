89d3bde60a0813ec0c4a33acf1f34bca2d152677
Hieu <hieun201104@gmail.com>
2025-09-14 15:28:40 -0700

    Feat: Display Events list on its own page pulled from backend

--- src/Pages/Members/Members.tsx
+++ src/Pages/Members/Members.tsx
@@ -1,24 +1,26 @@
 import { useEffect } from "react";
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import { useSiteHandler } from "../../Hooks/useSiteHandler";
 import { useAuthStore } from "../../Hooks/Auth/useAuthStore";
 import Auth from "./Auth/Auth";
 import Home from "./Home/Home";
+import EventsList from "./Events/EventsList";
 
 const Members = () => {
   const { authState } = useAuthStore();
   const { navigate } = useSiteHandler();
-  
+
   useEffect(() => {
     if (authState != "authenticated") navigate({ pathname: "/auth" });
   }, [authState]);
-  
+
   return (
     <Routes>
       <Route index element={<Home />} />
       <Route path="/auth" element={<Auth />} />
+      <Route path="/events" element={<EventsList />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default Members
+export default Members;
