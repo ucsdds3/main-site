7b6c01646c8a96aac056323c22797306172457e7
Jiaqi <jinjiaqi10220@gmail.com>
2025-11-24 17:33:05 -0800

    Fixed TritonBall link and fixed line spacing for About Us section

--- src/Pages/Consulting/Home/AboutUs.tsx
+++ src/Pages/Consulting/Home/AboutUs.tsx
@@ -28,7 +28,7 @@ const AboutUs = () => {
         <h2 className="text-[clamp(2.2rem,8vw,4rem)] md:text-[clamp(2.2rem,4vw,4rem)] font-semibold uppercase">
           About Us
         </h2>
-        <p className="text-[clamp(1.3rem,1.5vw,2rem)] opacity-85 indent-8 font-light md:leading-loose text-justify text-wrap px-[clamp(1rem,7vw,7rem)] md:px-0">
+        <p className="text-[clamp(1.3rem,1.5vw,2rem)] opacity-85 indent-8 font-light md:leading-medium text-justify text-wrap px-[clamp(1rem,7vw,7rem)] md:px-0">
         DS3 Consulting is a student-run team of data scientists and engineers offering end-to-end solutions in analytics, machine learning, MLOps, and infrastructure. We collaborate with organizations of all sizes to deliver custom insights, models, and scalable data products that drive impact. While we proudly offer pro-bono support to local small businesses and nonprofits, we also welcome paid partnerships and contract work with startups, research teams, and enterprise clients seeking reliable, high-quality technical solutions.
         </p>
       </div>
