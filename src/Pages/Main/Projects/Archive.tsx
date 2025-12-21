7a7d8d5f34991a12a683261baa4e4747fabd2a9a
Issac Roy <issacroy05@gmail.com>
2025-12-21 12:45:42 -0800

    added archive section

--- src/Pages/Main/Projects/Archive.tsx
+++ src/Pages/Main/Projects/Archive.tsx
@@ -1,18 +1,18 @@
-import HoverCard from "../../../Components/HoverCard";
-import Paginate from "../../../Components/Paginate";
-import Section from "../../../Components/Section"
-import projectsData from "../../../Assets/Data/projects.json";
-import { usePaginate } from "../../../Hooks/usePaginate";
 import { useState } from "react";
+import HoverCard from "../../../Components/HoverCard";
+import Section from "../../../Components/Section";
+import projectsData from "../../../Assets/Data/projects.json";
+import Paginate from "../../../Components/Paginate";
+import { usePaginate } from "../../../Hooks/usePaginate";
 
-const ShowCase = () => {
-  const projects = projectsData.projects;
-  type YearType = keyof typeof projects;
-  const years = Object.keys(projects).reverse() as YearType[];
+const Archive = () => {
+  const archive = projectsData.archive;
+  type YearType = keyof typeof archive;
+  const years = Object.keys(archive).reverse() as YearType[];
   const [year, setYear] = useState<YearType>(years[0]);
 
   const { page, setPage, numPages, start, end } = usePaginate({
-    totalItems: projects[year].length,
+    totalItems: archive[year].length,
     numRows: 1,
   });
 
@@ -20,8 +20,8 @@ const ShowCase = () => {
     <Section>
       <div className="flex flex-col md:flex-row gap-4 justify-between items-center w-full">
         <div className="flex flex-col text-center md:text-left">
-          <h2 className="text-5xl font-semibold">PROJECT SHOWCASE</h2>
-          <p className="text-2xl"> Here are some of our latest projects.</p>
+          <h2 className="text-5xl font-semibold">PROJECT ARCHIVE</h2>
+          <p className="text-2xl"> Here are some projects from previous years.</p>
         </div>
 
         <fieldset className="fieldset w-[clamp(10rem,15vw,15rem)] flex flex-col items-center gap-2">
@@ -42,7 +42,7 @@ const ShowCase = () => {
       </div>
 
       <div className="w-full grid grid-cols-[repeat(auto-fit,minmax(clamp(300px,40vw,350px),1fr))] justify-center gap-y-8">
-        {projects[year].slice(start, end).map((project, index) => (
+        {archive[year].slice(start, end).map((project, index) => (
           <HoverCard
             key={index}
             {...project}
@@ -55,7 +55,7 @@ const ShowCase = () => {
 
       <Paginate numPages={numPages} page={page} setPage={setPage} />
     </Section>
-  )
-}
+  );
+};
 
-export default ShowCase
+export default Archive;
