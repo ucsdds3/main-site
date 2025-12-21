ef7b1af0a02e163014f124137b0e16077b603adb
Issac Roy <issacroy05@gmail.com>
2025-12-21 15:03:12 -0800

    switched members and join us

--- src/Components/Page/Links.tsx
+++ src/Components/Page/Links.tsx
@@ -1,30 +1,32 @@
-import { useAuthStore } from "../../Hooks/Members/Auth/useAuthStore";
-import { useSiteHandler } from "../../Hooks/useSiteHandler";
-import navData from "../../Assets/Data/navbar.json";
-import NavItem from "./NavItem";
-import Avatar from "./Avatar";
+import { useAuthStore } from '../../Hooks/Members/Auth/useAuthStore';
+import { useSiteHandler } from '../../Hooks/useSiteHandler';
+import navData from '../../Assets/Data/navbar.json';
+import NavItem from './NavItem';
+import Avatar from './Avatar';
 
 const Links = ({ menuOpen }: { menuOpen: boolean }) => {
   const { authState } = useAuthStore();
   const { subdomain, navigate } = useSiteHandler();
   const links = navData[subdomain as keyof typeof navData] || navData.main;
-  const typographyClasses = "font-quicksand font-normal tracking-[0px]";
+  const typographyClasses = 'font-quicksand font-normal tracking-[0px]';
 
   return (
     <div
       className={`w-full lg:w-auto items-center flex-col lg:flex lg:flex-row gap-6 text-2xl ${
-        menuOpen ? "flex" : "hidden"
+        menuOpen ? 'flex' : 'hidden'
       } ${typographyClasses}`}
     >
       {Object.entries(links).map(([label, data], index) => (
         <NavItem key={index} label={label} data={data} />
       ))}
-      {subdomain == "main" || authState == "signin" ? (
+      {subdomain == 'main' || authState == 'signin' ? (
         <button
-          onClick={subdomain == "main" ? () => navigate({ pathname: "/join-us" }) : () => navigate({ pathname: "/", subdomain: "main" })}
+          onClick={() =>
+            navigate({ pathname: '/', subdomain: subdomain == 'main' ? 'members' : 'main' })
+          }
           className={`bg-(--color-primary) px-4 pb-2 pt-1 hover:brightness-110 cursor-pointer rounded-full text-center ${typographyClasses} font-medium w-full sm:w-auto min-w-[120px] uppercase`}
         >
-          {subdomain == "main" ? "Join Us" : "Main Site"}
+          {subdomain == 'main' ? 'Members Login' : 'Main Site'}
         </button>
       ) : (
         <Avatar />
