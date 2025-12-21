8a6c33662e7aa1ac895241f2b8a3ecb16dc08ad6
Issac Roy <issacroy05@gmail.com>
2025-12-21 13:33:18 -0800

    fix: import

--- src/Pages/Main/Projects/Projects.tsx
+++ src/Pages/Main/Projects/Projects.tsx
@@ -1,22 +1,22 @@
-import { lazy, Suspense, useRef } from "react";
-import Page from "../../../Components/Page/Page.tsx";
-import Landing from "./Landing.tsx";
+import { lazy, Suspense, useRef } from 'react'
+import Page from '../../../Components/Page/Page.tsx'
+import Landing from './Landing.tsx'
 const About = lazy(() => {
-  return import("../../../Components/About.tsx");
-});
+  return import('../../../Components/About.tsx')
+})
 const Archive = lazy(() => {
-  return import("./Archive.tsx");
-});
+  return import('./Archive.tsx')
+})
 const Gallery = lazy(() => {
-  return import("../../../Components/Gallery.tsx");
-});
+  return import('../../../Components/Gallery.tsx')
+})
 
-import projects from "../../../Assets/Data/projects.json";
-import ShowCase from "./Showcase.tsx";
+import projects from '../../../Assets/Data/projects.json'
+import ShowCase from './Showcase'
 
 const Projects = () => {
-  const scrollRef = useRef<HTMLDivElement>(null!);
-  
+  const scrollRef = useRef<HTMLDivElement>(null!)
+
   return (
     <Page scrollRef={scrollRef}>
       <Landing />
@@ -29,7 +29,7 @@ const Projects = () => {
         </Suspense>
       </div>
     </Page>
-  );
-};
+  )
+}
 
-export default Projects;
+export default Projects
