c52de7b34df93aa10d02361581aa762264d2843f
Issac Roy <issacroy05@gmail.com>
2026-01-06 17:02:52 -0800

    fix: some bs #prod

--- src/Hooks/Members/Auth/useForgotPassword.ts
+++ src/Hooks/Members/Auth/useForgotPassword.ts
@@ -23,7 +23,6 @@ export function useForgotPassword() {
     }
 
     const href = window.location.href;
-    const search = new URLSearchParams(window.location.search);
     const { data, error } = await supabase.auth.resetPasswordForEmail(overrideEmail || email, {
       redirectTo: `${href}?authState=reset-password`,
     });
