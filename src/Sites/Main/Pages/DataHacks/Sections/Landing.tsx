8074327dbd2d7e10807dacd8ac0190cc118057b3
Issac Roy <issacroy05@gmail.com>
2026-01-22 18:37:37 -0800

    refactor: main site

---
+++ src/Sites/Main/Pages/DataHacks/Sections/Landing.tsx
@@ -0,0 +1,38 @@
+import Section from "src/Shared/Page/Section.tsx";
+import Button from "src/Shared/Components/Button.tsx";
+import { getNextDeadline } from "src/Utils/functions.tsx";
+
+import datahacks from "../Data/datahacks.json";
+import Countdown from "../Components/Countdown.tsx";
+
+const Landing = () => {
+  const deadline = getNextDeadline(datahacks.deadlines);
+
+  return (
+    <Section className="h-[90vh] justify-between">
+      <div className="flex flex-col text-center lg:text-left px-10">
+        <h2 className="text-[clamp(1.5rem,2vw,2rem)] font-medium">What Will You Create?</h2>
+        <h1 className="text-[clamp(2.7rem,5vw,5rem)] font-medium leading-tight uppercase text-glow tracking-wider">
+          {/* This school year*/}
+          {`DataHacks ${new Date().getFullYear() + (new Date().getMonth() > 9 ? 1 : 0)}`}
+        </h1>
+        <p className="text-[clamp(1.1rem,1.5vw,1.4rem)]">
+          Analyze real-world data, build powerful insights, and create impactful solutions.
+        </p>
+      </div>
+
+      <Countdown />
+      {deadline && deadline[0].toLowerCase().includes("apply") && (
+        <Button
+          onClick={() => {
+            window.open(datahacks.application, "_blank");
+          }}
+        >
+          APPLY NOW
+        </Button>
+      )}
+    </Section>
+  );
+};
+
+export default Landing;
