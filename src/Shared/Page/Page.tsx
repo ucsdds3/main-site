52f928ebc3c5348902c5cb21b8398b6073c9bf47
Issac Roy <issacroy05@gmail.com>
2026-01-22 18:26:35 -0800

    hella refactoring

---
+++ src/Shared/Page/Page.tsx
@@ -0,0 +1,39 @@
+import "simplebar-react/dist/simplebar.min.css";
+import SimpleBar from "simplebar-react";
+
+import bear from "src/Assets/Images/Sleepy_Bear.webp";
+
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import ScrollArrow from "./ScrollArrow";
+
+interface PageProps extends React.HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+  scrollRef?: React.RefObject<HTMLDivElement>;
+}
+
+const Page = ({ children, scrollRef, ...props }: PageProps) => {
+  return (
+    <SimpleBar className="absolute left-0 top-0 w-screen h-screen overflow-x-hidden">
+      <Navbar />
+      <div className="flex flex-col items-center min-h-[80vh]" {...props}>
+        {scrollRef && <ScrollArrow ref={scrollRef} />}
+        {children || (
+          <>
+            <h2 className="text-center hero-text-shadow my-10 text-[clamp(2rem,10vw,4rem)]">
+              Coming Soon
+            </h2>
+            <img
+              src={bear}
+              alt="Under Construction"
+              className="bear mt-[clamp(5rem,5vw,10rem)] w-[clamp(20rem,60vw,65rem)]"
+            />
+          </>
+        )}
+      </div>
+      <Footer />
+    </SimpleBar>
+  );
+};
+
+export default Page;
