01b78b232f16299849f79ad5a390e9d1a1bd0eb1
Issac Roy <issacroy05@gmail.com>
2026-01-22 22:55:56 -0800

    fix: various things

--- src/Hooks/useSignOut.ts
+++ src/Hooks/useSignOut.ts
@@ -1,6 +1,6 @@
 import { toast } from "react-hot-toast";
-import { supabase } from "../../../Utils/supabase";
-import { useAuthStore } from "../../../Sites/Members/Hooks/useAuthStore";
+import { supabase } from "src/Utils/supabase";
+import { useAuthStore } from "src/Sites/Members/Hooks/useAuthStore";
 
 export function useSignOut() {
   const { setAuthState, setUser, setAdminLevel } = useAuthStore();
