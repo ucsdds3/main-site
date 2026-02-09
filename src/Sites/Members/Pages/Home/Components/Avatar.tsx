7e98f40057da76a022586bb3b3910fc24b4c3d1d
Issac Roy <issacroy05@gmail.com>
2026-02-09 12:28:01 -0800

    fix: use user profile picture before rand_dino

--- src/Sites/Members/Pages/Home/Components/Avatar.tsx
+++ src/Sites/Members/Pages/Home/Components/Avatar.tsx
@@ -5,22 +5,27 @@ import projects_dino from "src/Assets/Images/projects_dino.webp";
 import dino from "src/Assets/Images/dino_square.webp";
 
 import { useStats } from "../Hooks/useStats";
+import { useAuthStore } from "../../../Hooks/useAuthStore";
 
 const Avatar = () => {
   const { progress, tier } = useStats();
+  const { user } = useAuthStore();
 
   const rand_dino = useMemo(() => {
     const dinos = [dino, contact_dino, projects_dino];
     return dinos[Math.floor(Math.random() * dinos.length)];
   }, []);
 
+  const profilePicture = user?.user_metadata?.profile_picture;
+  const avatarSrc = profilePicture || rand_dino;
+  const avatarAlt = profilePicture ? "Profile picture" : "Dino";
+
   return (
     <div
       className={`size-60 p-8 bg-base-300 radial-progress ${tier.color}`}
       style={{ "--value": progress * 100 } as React.CSSProperties}
     >
-      {/* TODO: User profile picture instead of random dino */}
-      <img src={rand_dino} alt="Dino" />
+      <img src={avatarSrc} alt={avatarAlt} />
     </div>
   );
 };
