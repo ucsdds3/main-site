5b69ec6af86e423ba2581932a6193b2bb31dca2c
DanielM7852 <dandan7852@gmail.com>
2026-02-10 13:02:48 -0800

    Paginated two per row

--- src/Shared/Components/Paginate.tsx
+++ src/Shared/Components/Paginate.tsx
@@ -9,17 +9,19 @@ interface PaginateProps {
 const Paginate = ({ numPages, page, setPage }: PaginateProps) => {
   return (
     numPages > 1 && (
-      <div className="join">
-        {newArray(numPages).map((_, index) => (
-          <button
-            key={index}
-            className="join-item btn data-[active=true]:btn-active btn-lg text-xl"
-            onClick={() => setPage(index + 1)}
-            data-active={page == index + 1}
-          >
-            {index + 1}
-          </button>
-        ))}
+      <div className="flex justify-center w-full pt-8">
+        <div className="join">
+          {newArray(numPages).map((_, index) => (
+            <button
+              key={index}
+              className="join-item btn data-[active=true]:btn-active btn-lg text-xl"
+              onClick={() => setPage(index + 1)}
+              data-active={page == index + 1}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
       </div>
     )
   );
