bdc01e7bd5654c098fb63bc0929c7cfdb7cff580
Hieu <vin028@ucsd.edu>
2026-02-19 17:49:56 -0800

    Feat: Added skeleton pie chart to admin page with export functionality for underlying data

--- src/Sites/Members/Pages/Admin/Components/DashboardStatsStrip.tsx
+++ src/Sites/Members/Pages/Admin/Components/DashboardStatsStrip.tsx
@@ -1,13 +1,17 @@
 import { useAdminStats } from "../Hooks/useAdminStats";
+import DashboardPieChart from "./DashboardPieChart";
 
 export default function DashboardStatsStrip() {
   const stats = useAdminStats();
 
   return (
-    <div className={`flex w-full flex-wrap gap-4 ${Object.keys(stats).length}`}>
-      {Object.entries(stats).map(([label, stats]) => (
-        <StatCard key={label} label={label} {...stats} />
-      ))}
+    <div className="flex w-full">
+      <div className={`flex w-2/3 flex-wrap gap-4  ${Object.keys(stats).length}`}>
+        {Object.entries(stats).map(([label, stats]) => (
+          <StatCard key={label} label={label} {...stats} />
+        ))}
+      </div>
+      <DashboardPieChart />
     </div>
   );
 }
@@ -20,7 +24,7 @@ interface StatCardProps {
 }
 function StatCard({ label, value, hint, loading }: StatCardProps) {
   return (
-    <div className="flex-1 min-w-[200px] rounded-2xl bg-base-300 p-5 border border-base-content/50">
+    <div className="flex-1 min-w-[300px] rounded-2xl bg-base-300 p-5 border border-base-content/50">
       <div className="text-lg font-semibold tracking-wide">{label}</div>
       <div className="mt-2 text-3xl font-semibold text-primary">
         {loading ? <span className="loading loading-dots loading-sm" /> : value}
