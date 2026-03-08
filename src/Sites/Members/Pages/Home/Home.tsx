09177961f8086e2d6c388fbac3e25fcc6ef024f9
Hieu <hieun201104@gmail.com>
2026-03-08 12:05:21 -0700

    Feat: Top 10 and podium on the home page

--- src/Sites/Members/Pages/Home/Home.tsx
+++ src/Sites/Members/Pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import Page from "src/Shared/Page/Page";
 import Events from "./Sections/Events";
 import Header from "./Sections/Header";
+import Leaderboard from "./Sections/Leaderboard";
 
 const Home = () => {
   return (
     <Page>
       <Header />
       <Events />
+      <Leaderboard />
     </Page>
   );
 };
