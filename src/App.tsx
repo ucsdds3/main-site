2d21129d7464dd9432dfd5eab291198565a4cfcf
Issac Roy <issacroy05@gmail.com>
2026-03-31 19:50:31 -0700

    feat: removed consulting site #prod

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useThemeHandler } from "./Hooks/useThemeHandler";
 import { useSiteHandler } from "./Hooks/useSiteHandler";
 import { useAuth } from "./Hooks/useAuth";
 
-import Consulting from "./Sites/Consulting/Consulting";
 import Members from "./Sites/Members/Members";
 import Main from "./Sites/Main/Main";
 
@@ -12,7 +11,6 @@ const App = () => {
   const { subdomain } = useSiteHandler();
 
   const sites = {
-    consulting: <Consulting />,
     members: <Members />,
     main: <Main />,
   };
