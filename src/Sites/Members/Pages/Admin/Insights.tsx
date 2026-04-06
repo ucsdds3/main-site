2e7ac85dc3e6aac542a2a205243d4369cefbab83
Issac Roy <issacroy05@gmail.com>
2026-04-06 15:09:23 -0700

    refactor: moved charts to insight component

--- src/Sites/Members/Pages/Admin/Insights.tsx
+++ src/Sites/Members/Pages/Admin/Insights.tsx
@@ -1,11 +1,23 @@
 import Page from "src/Shared/Page/Page";
 import DashboardStatsStrip from "./Components/DashboardStatsStrip";
+import DashboardLineChart from "./Components/DashboardLineChart";
+import DashboardPieChart from "./Components/DashboardPieChart";
 
 export default function Insights() {
   return (
     <Page data-theme="dark">
       <div className="mx-auto max-w-[1800px] px-6 py-8 w-full">
-        <DashboardStatsStrip />
+        <div className="flex w-full flex-col gap-4">
+          <DashboardStatsStrip />
+          <div className="flex flex-wrap flex-1 min-h-[420px] w-full items-stretch gap-0 justify-center">
+            <div className="min-w-[max(33%,300px)]  flex flex-col flex-1 min-h-0">
+              <DashboardPieChart />
+            </div>
+            <div className="min-w-[max(67%,300px)] flex flex-col flex-1 min-h-0">
+              <DashboardLineChart />
+            </div>
+          </div>
+        </div>
       </div>
     </Page>
   );
