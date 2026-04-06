821d47786d9ae117be5c912548274e18ebfaa307
Issac Roy <issacroy05@gmail.com>
2026-04-06 15:19:45 -0700

    refactor: moved leaderboard to its own page

--- src/Sites/Members/Pages/Home/Home.tsx
+++ src/Sites/Members/Pages/Home/Home.tsx
@@ -1,14 +1,12 @@
 import Page from "src/Shared/Page/Page";
 import Events from "./Sections/Events";
 import Header from "./Sections/Header";
-import Leaderboard from "./Sections/Leaderboard";
 
 const Home = () => {
   return (
     <Page>
       <Header />
       <Events />
-      <Leaderboard />
     </Page>
   );
 };
